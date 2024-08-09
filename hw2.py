9d9e6b8f114571896a5064942a904763747106ef
shivanijl <shivanig.jetlearn@gmail.com>
2024-08-09 23:24:26 +0530

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,12 @@
+#create dictonary 
+textbook = {
+    "name":"Algebra", 
+    "price":"540"
+    
+}
+
+print(textbook)
+
+# Accessing values in a dictionary
+textbook["price"] = "600"
+print(textbook)
